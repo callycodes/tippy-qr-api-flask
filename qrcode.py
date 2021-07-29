403dcdb38392552f109e1b0e694556d761c6cc79
Cal <61029674+callycodes@users.noreply.github.com>
2021-07-29 08:30:45 +0100

    Refactoring, QR Code Service
    Removal of all TypeDB refs, user endpoints, as this Flask service is being used only for QR generation.
    
    Instead the authentication, models and services will be handled by the nodejs backend.

--- qrcode.py
+++ qrcode.py
@@ -2,24 +2,28 @@ import qrlib
 from PIL import Image
 import cairosvg
 from io import BytesIO
+import base64
 
-text = 'rebeka hi'
+def createQR():
+  text = 'rebeka hi'
+  one_qr = qrlib.generate_custom_qr_file(text, language='en', qr_format='PNG',
+                  size='1000', ec_level='H', instructions=False,
+                  style='sieve', style_color='#000000',
+                  inner_eye_style='leaf', inner_eye_color='#000000',
+                  outer_eye_style='left_eye', outer_eye_color='#000000',
+                  bg_color='#FFFFFF')
 
-one_qr = qrlib.generate_custom_qr_file(text, language='en', qr_format='PNG',
-                 size='1000', ec_level='H', instructions=False,
-                 style='sieve', style_color='#000000',
-                 inner_eye_style='leaf', inner_eye_color='#000000',
-                 outer_eye_style='left_eye', outer_eye_color='#000000',
-                 bg_color='#FFFFFF')
+  qr = Image.open(one_qr).convert('RGBA')
 
-qr = Image.open(one_qr).convert('RGBA')
+  file_o = open('tippy.svg')
 
+  out = BytesIO()
+  cairosvg.svg2png(file_obj=file_o, write_to=out)
+  image = Image.open(out).convert('RGBA').resize((250, 150))
 
-file_o = open('tippy.svg')
-
-out = BytesIO()
-cairosvg.svg2png(file_obj=file_o, write_to=out)
-image = Image.open(out).convert('RGBA').resize((250, 150))
-
-qr.paste(image, (380, 410), image)
-qr.show()
+  qr.paste(image, (380, 410), image)
+  byte_arr = BytesIO()
+  qr.save(byte_arr, format='PNG')
+  
+  out = "data:image/png;base64," + base64.b64encode(byte_arr.getvalue()).decode('ascii')
+  return out
